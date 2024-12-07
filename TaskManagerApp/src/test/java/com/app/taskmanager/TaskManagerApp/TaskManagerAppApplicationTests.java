c7677162e17c4045b7b197432a967a8fad893466
Ranjana Shenoy <ranjana.shenoyk@gmail.com>
2024-12-07 15:15:16 +0530

    Initialise Task Manager App. First Commit in the repo.

---
+++ TaskManagerApp/src/test/java/com/app/taskmanager/TaskManagerApp/TaskManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.taskmanager.TaskManagerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
